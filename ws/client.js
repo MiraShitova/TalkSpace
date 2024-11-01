1e301f057cdcac23857932585809a20c26ea8b7b
Myroslava <m.shitova@istu.edu.ua>
2024-11-01 15:20:41 +0200

    WebSocket

--- ws/client.js
+++ ws/client.js
@@ -1,20 +1,36 @@
-const { WebSocket } = require('ws');
+const readline = require('node:readline');
 
-const wsClientFactory = (id) => {
-    const ws = new WebSocket('ws://localhost:8080');
+const { ChatClient } = require('./client/ChatClient');
 
-    ws.on('error', console.error);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-    ws.on('open', function open() {
-        console.log(`connected ${id}`);
-        ws.send(`${id} HELLO`);
+
+rl.question(`What's your name?` , name => {
+    rl.close();
+    init(name);
+});
+
+
+const init = (name) => {
+    const client = new ChatClient({ url: 'ws://localhost:8080', username: name });
+
+    client.init();
+
+    const chatInput = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
     });
 
-    ws.on('message', function message(data) {
-        console.log(`${id} received: %s`, data);
+    chatInput.on('line', (input) => {
+        if (input.trim().toLowerCase() === 'exit') {
+            chatInput.close();
+        } else {
+            client.send(input);
+        }
     });
 }
+  
 
-const wsClient1 = wsClientFactory(1);
-const wsClient2 = wsClientFactory(2);
-const wsClient3 = wsClientFactory(3);
