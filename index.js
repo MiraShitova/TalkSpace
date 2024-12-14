f8bcb9b3391c32c07cd001de01ab2e81572501aa
Myroslava <m.shitova@istu.edu.ua>
2024-12-14 14:51:12 +0200

    task 8

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
-app.use(express.static(path.join(_dirname, 'pages')))
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(3000, () => {
-console.log('Server is running on port 3000')
-})
+// Віддаємо статичні файли
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Події Socket.io
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // Обробка повідомлень від клієнтів
+    socket.on('chat message', (msg) => {
+        console.log('Message received:', msg);
+        io.emit('chat message', msg); // Відправляємо повідомлення всім
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
+
+// Запускаємо сервер
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
